e3d438e028e18b83523ed6398a86a4e643646750
Tanmoy Krishna Das <tanmoykrishnadas@gmail.com>
2019-01-01 15:32:52 +0600

    Initial commit

---
+++ faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "ZI_6IKxIqPRO_aMm33DDSoUwljEK3HuE";
+	public static String API_SECRET = "-U53IcBS82OlVaxVaxMPNHdeHOK2UUe7";
+
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
+}
